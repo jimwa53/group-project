bdeaa2fbff3d8ee50801ea1a15bcfe996e01128c
N1cafk <56705321+n1cafk@users.noreply.github.com>
2019-11-28 15:18:15 +0800

    Update booking.js

--- javascript/booking.js
+++ javascript/booking.js
@@ -35,3 +35,18 @@ function checkined(roomid) {
     }
     
 }
+
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+
+for (let item of anchorlinks) {
+    item.addEventListener('click', (e)=> {
+		let hashval = item.getAttribute('href')
+		let target = document.querySelector(hashval)
+		target.scrollIntoView({
+			behavior: 'smooth'
+		})
+		history.pushState(null, null, hashval)
+		e.preventDefault()
+	})
+}
+}

114dc726c431b8cdb88d77444c735da081d740a5
IamWaishing <waishing2702@gmail.com>
2019-11-28 18:23:40 +0800

    28Nov2019 - 18:23

--- javascript/booking.js
+++ javascript/booking.js
@@ -4,44 +4,36 @@ var checkOutDate = document.getElementById("checkout");
 
     
 function searchBooking(){
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     date1 = moment($("#checkin").val(),"YYYY-MM-DD");
     date2 = moment($("#checkout").val(),"YYYY-MM-DD");
+	var timeover;
+	function compare(dateTimeA, dateTimeB) {
+	if (date1 < date2) {
+		return timeover = 1;
+	} else if (date1 > date2) {
+		return timeover = -1;
+	} else {
+		return timeover = 0;
+	}
+	}
     if(checkInDate.value == "" || checkOutDate.value == "")
     {
         return alert("Please choose you booking date");
-    }else
-    {
-        for(i=0;i<roomlist.length;i++)
-       { 
-            if(moment(roomlist[i].indate).isBetween(date1,date2, null,'[]') || moment(roomlist[i].outdate).isBetween(date1,date2, null,'[]'))
-            {
-                var room = roomlist[i].roomId;
-                
-                alert("The room "+(i+1)+"is booked")
-            }else
-            {
-                //alert("The room "+(i+1)+" can booked")
-                var UserId = roomlist[i].UserId;
-                var roomId = roomlist[i].roomId;
-                var indate = roomlist[i].indate;
-                var outdate = roomlist[i].outdate;
-
-                var htmlContent =
-                "<div class=\"col-12\">"+roomId+" can book</div>"
-
-                $("#bookinglist").append(htmlContent)
-
-            }
-        }
+    }else if(compare($("#checkin").val(),$("#checkout").val()) == -1){
+		alert("The checkout day should be greater than "+$("#checkin").val());
     }  
 }
 
+function bookcheck(r) {
+	console.log(r);
+    date1 = moment($("#checkin").val(),"YYYY-MM-DD");
+    date2 = moment($("#checkout").val(),"YYYY-MM-DD");
+				if(moment(roomlist[r-1].indate).isBetween(moment(roomlist[r-1].indate),moment(roomlist[r-1].outdate), null, '[]'))
+				{
+					var room = roomlist[r-1].roomId;
+
+					alert("The room "+(r)+" is booked")
+					document.getElementById(r+String(r)).setAttribute("disabled", "");
+				}
+}

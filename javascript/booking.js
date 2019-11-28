681bae2e3a14bccd2437c9ffc6dfeef8d98e13c2
jim <jim@smart_int.com>
2019-11-28 16:04:25 +0800

    booking

--- javascript/booking.js
+++ javascript/booking.js
@@ -1,37 +1,47 @@
+var checkInDate = document.getElementById("checkin");
+var checkOutDate = document.getElementById("checkout");
 
-var checkInDate = document.getElementById("checkIn");
-    var checkOutDate = document.getElementById("checkIn");
+
+    
+function searchBooking(){
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    date1 = moment($("#checkin").val(),"YYYY-MM-DD");
+    date2 = moment($("#checkout").val(),"YYYY-MM-DD");
     if(checkInDate.value == "" || checkOutDate.value == "")
     {
-        return alert("Please")
-    }
-function checkined(roomid) {
-    if (sessionStorage.getItem("logineduser")==null) {
-        var islogined = confirm("Please sign in first!");
-        if (islogined==true) {
-            window.location.href="signin.html";
-            sessionStorage.setItem("url",document.location);
+        return alert("Please choose you booking date");
+    }else
+    {
+        for(i=0;i<roomlist.length;i++)
+       { 
+            if(moment(roomlist[i].indate).isBetween(date1,date2, null,'[]') || moment(roomlist[i].outdate).isBetween(date1,date2, null,'[]'))
+            {
+                var room = roomlist[i].roomId;
+                
+                alert("The room "+(i+1)+"is booked")
+            }else
+            {
+                //alert("The room "+(i+1)+" can booked")
+                var UserId = roomlist[i].UserId;
+                var roomId = roomlist[i].roomId;
+                var indate = roomlist[i].indate;
+                var outdate = roomlist[i].outdate;
+
+                var htmlContent =
+                "<div class=\"col-12\">"+roomId+" can book</div>"
+
+                $("#bookinglist").append(htmlContent)
+
+            }
         }
     }
-    else
-    {
-        var sure = confirm("Sure?");
-        if (sure==true) {
-            rm = document.getElementById(roomid).id;
-            roomlist.forEach(function(obj) {
-                if(obj.roomId==rm) {
-                    obj.UserId = signinusername;
-                    var indexofnum = roomlist.indexOf(obj);
-                    roomlist[indexofnum] = {
-                                                UserId:signinusername,
-                                                roomId:rm,
-                                                indate:"",
-                                                outdate:""
-                                            }
-                    localStorage.setItem("roomlist",JSON.stringify(roomlist));
-                }
-            })
-            window.location.href="confirmroom.html";
-    }
-    
 }
+

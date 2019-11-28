2ef347561658619fb1dc7a93d6dc6905d1297760
IamWaishing <waishing2702@gmail.com>
2019-11-28 13:49:12 +0800

    28Nov2019 13:49

--- javascript/booking.js
+++ javascript/booking.js
@@ -35,17 +35,3 @@ function checkined(roomid) {
     }
     
 }
-
-let anchorlinks = document.querySelectorAll('a[href^="#"]')
-
-for (let item of anchorlinks) {
-    item.addEventListener('click', (e)=> {
-		let hashval = item.getAttribute('href')
-		let target = document.querySelector(hashval)
-		target.scrollIntoView({
-			behavior: 'smooth'
-		})
-		history.pushState(null, null, hashval)
-		e.preventDefault()
-	})
-}
